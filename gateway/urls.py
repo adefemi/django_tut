79a1bd3ea6a884978110a0eca336cb09cf385875
Adefemi <adefemi.oseni@algorimsng.com>
2020-06-23 13:24:19 +0100

    updated

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from .views import LoginView, RegisterView, RefreshView, TestException
+from .views import LoginView, RegisterView, RefreshView
 
 urlpatterns = [
     path('login', LoginView.as_view()),
     path('register', RegisterView.as_view()),
     path('refresh', RefreshView.as_view()),
-    path('test-exc', TestException.as_view()),
 ]
